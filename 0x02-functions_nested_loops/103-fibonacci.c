5e2b1317c490bfe78ba670ee463a0f327730528b
CharityWoods <charitymapulanga13@gmail.com>
2023-02-21 17:19:34 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,9 +4,12 @@
  * Description: computes and prints even number < 4,000,000
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
- */int main(void)
+ */
+
+int main(void)
 {
 	int a = 0, b = 1, next = 0;
+
 	int sum = 0;
 
 	while (next < 4000000)
@@ -17,6 +20,6 @@
 		if (next % 2 == 0)
 			sum += next;
 	}
-	printf("%i\n", sum);
+	printf('%d\n', sum);
 	return (0);
 }
