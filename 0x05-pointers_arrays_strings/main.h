718f2f74163b8a29233a7199623d8aa8940dc1ba
CharityWoods <charitymapulanga13@gmail.com>
2023-02-27 05:51:32 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -24,5 +21,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
-
-#endif
