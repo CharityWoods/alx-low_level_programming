f59f62c5f2bbd3c2459edcf62340ccfcaae6d93a
CharityWoods <charitymapulanga13@gmail.com>
2023-03-08 15:00:30 -0800

    3-factorial.c

--- 0x08-recursion/2-main.c
+++ 0x08-recursion/2-main.c
@@ -11,6 +11,8 @@ int main(void)
 	int n;
 
 	n = _strlen_recursion("Corbin Coleman");
+
 	printf("%d\n", n);
+
 	return (0);
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,17 +2,19 @@
 
 /**
  * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
+ * @s: The string to count
  *
- * Return: The length of the string.
+ * Return: integer value
  */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s)
+	if (*s == '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (0);
 	}
+
+	s++;
+	return (_strlen_recursion(s + 1);
 }
